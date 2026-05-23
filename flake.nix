920d4ad8c18e82f7e36bcec7c0553ccb15bcda85
Victor Jacobs <victor@vjcbs.be>
2026-05-23 12:03:26 +0200

    Add Nix flake with Go 1.26

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Development environment for mealie-importer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.go_1_26;
+          go = pkgs.go_1_26;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go_1_26
+            ];
+          };
+        }
+      );
+    };
+}
